7096867ded9821ed8c47769f699b759c87c5a13b
muhammad-abdul-rehman <muhammad_abdul_rehman@nextpak.org>
2022-06-30 00:21:40 +0500

    Filter Changes

--- src/components/Search Form/MyEventCards.js
+++ src/components/Search Form/MyEventCards.js
@@ -9,36 +9,48 @@ import {
 
 const MyEventCards = (props) => {
   return (
-    <>
-      <CardGroup>
-        <Card>
-          <CardBody>
-            <CardTitle>
-              Event Title : {props.data ? props.data.title : "OrganizerName"}
-            </CardTitle>
-            <CardSubtitle>
-              Catagory : {props.data ? props.data.category : "0.0"}
-            </CardSubtitle>
-            <CardSubtitle>
-              Price :{props.data ? props.data.price : "OrganizerName"}
-            </CardSubtitle>
-            <CardSubtitle>
-              Location : pak
-              {props.data ? props.data.location : "Props Catagory"}
-            </CardSubtitle>
-            <CardSubtitle>
-              Organizer's Name :{" "}
-              {props.data ? props.data.organizer_name : "Location Empty"}
-            </CardSubtitle>
-            <CardSubtitle>
-              Addressed to :{" "}
-              {props.data ? props.data.addressed_to : "AddressNotFound"}
-            </CardSubtitle>
-            <CardSubtitle>Topic: dslk</CardSubtitle>
-          </CardBody>
-        </Card>
-      </CardGroup>
-    </>
+		<>
+			<CardGroup>
+				<Card>
+					<CardBody>
+						<CardTitle>
+							Event Title :{' '}
+							{props.data ? props.data.title : 'OrganizerName'}
+						</CardTitle>
+						<CardSubtitle>
+							Catagory :{' '}
+							{props.data ? props.data.category : '0.0'}
+						</CardSubtitle>
+						<CardSubtitle>
+							Price :
+							{props.data ? props.data.price : 'OrganizerName'}
+						</CardSubtitle>
+						<CardSubtitle>
+							Location :
+							{props.data
+								? props.data.location
+								: 'Props Catagory'}
+						</CardSubtitle>
+						<CardSubtitle>
+							Organizer's Name :{' '}
+							{props.data
+								? props.data.organizer_name
+								: 'Location Empty'}
+						</CardSubtitle>
+						<CardSubtitle>
+							Addressed to :{' '}
+							{props.data
+								? props.data.addressed_to
+								: 'AddressNotFound'}
+						</CardSubtitle>
+						<CardSubtitle>
+							Topic:{' '}
+							{props.data ? props.data.topic : 'TopicNotFound'}
+						</CardSubtitle>
+					</CardBody>
+				</Card>
+			</CardGroup>
+		</>
   );
 };
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,55 +1,62 @@
-import React from "react";
-import Navbar from "./NavigationBar";
-import Topbar from "./Search Form/Topbar";
-import SearchSidebar from "./Search Form/searchSidebar";
-import SearchFormCards from "./Search Form/SearchFormCards";
-import Footer from "./Footer";
+import React, { useEffect } from 'react';
+import Navbar from './NavigationBar';
+import Topbar from './Search Form/Topbar';
+import SearchSidebar from './Search Form/searchSidebar';
+import SearchFormCards from './Search Form/SearchFormCards';
+import Footer from './Footer';
 
 // import "../css/Search.css";
-import { useState } from "react";
-
+import { useState } from 'react';
 
 function Search() {
-  const [search, setSearch] = useState("");
-  const [Category, setCategory] = useState([]);
-  const [priceRange, setPriceRange] = useState([0, 20]);
-  console.log("Category", Category);
+	const [search, setSearch] = useState('');
+	const [Category, setCategory] = useState([]);
+	const [filter, setFilter] = useState({
+		category: '',
+		location: '',
+		addressTo: '',
+		topic: '',
+	});
+	const [priceRange, setPriceRange] = useState([0, 0]);
+	console.log('Category', Category);
+	useEffect(() => {
+		console.log({ filter });
+	}, [filter]);
 
-  return (
-    
-    <div className="container main__container">
-    
-      <div className="row sticky-top">
-        <Topbar search={search} setSearch={setSearch} />
-      </div>
-      <br />
-      <div className="row">
-        <div className="col-lg-3">
-          {" "}
-          {/* Inside Search Form */}
-          <SearchSidebar
-            Catagory={Category}
-            setCategory={setCategory}
-            priceRange={priceRange}
-            setPriceRange={setPriceRange}
-          />
-        </div>
-        <div className="col-lg-9">
-          {/* Inside Search Form */}
-          <SearchFormCards
-            search={search}
-            setSearch={setSearch}
-            Catagory={Category}
-            setCategory={setCategory}
-            priceRange={priceRange}
-          />
-        </div>
-      </div>
-   
-    </div>
-     
-   
-  );
+	return (
+		<div className='container main__container'>
+			<div className='row sticky-top'>
+				<Topbar search={search} setSearch={setSearch} />
+			</div>
+			<br />
+			<div className='row'>
+				<div className='col-lg-3'>
+					{' '}
+					{/* Inside Search Form */}
+					<SearchSidebar
+						Catagory={Category}
+						setCategory={setCategory}
+						priceRange={priceRange}
+						setPriceRange={setPriceRange}
+						filter={filter}
+						setFilter={setFilter}
+					/>
+				</div>
+				<div className='col-lg-9'>
+					{/* Inside Search Form */}
+					<SearchFormCards
+						search={search}
+						setSearch={setSearch}
+						Catagory={Category}
+						setCategory={setCategory}
+						priceRange={priceRange}
+						filter={filter}
+						setFilter={setFilter}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Search;
